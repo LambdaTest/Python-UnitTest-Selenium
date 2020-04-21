3a26477b61ce80ea6d3627f1217227a0cc510f50
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2020-04-21 20:21:27 +0530

    fixes

--- lambdatest_test.py
+++ lambdatest_test.py
@@ -1,15 +1,37 @@
-import unittest, time, re
-import base_test
-from selenium.webdriver.common.keys import Keys
+import os
+import unittest
+import sys
+from selenium import webdriver
 
-class LambdaTest(base_test.BaseTest):
+username = os.environ.get("LT_USERNAME")
+access_key = os.environ.get("LT_ACCESS_KEY")
+
+class FirstSampleTest(unittest.TestCase):
+
+    # setUp runs before each test case
+    def setUp(self):
+        desired_caps = {
+            "build": 'unittest sample build',
+            "name": 'Py-unittest',
+            "platform": 'Windows 10',
+            "browserName": 'firefox',
+            "version": '73'
+        }
+        self.driver = webdriver.Remote(
+           command_executor="http://{}:{}@hub.lambdatest.com:80/wd/hub".format(username, access_key),
+           desired_capabilities= desired_caps)
+
+
+# tearDown runs after each test case
+    def tearDown(self):
+        self.driver.quit()
 
     def test_unit_user_should_able_to_add_item(self):
         # try:
         driver = self.driver
 
         # Url
-        driver.get(self.base_url)
+        driver.get("https://lambdatest.github.io/sample-todo-app/")
 
         # Click on check box
         check_box_one = driver.find_element_by_name("li1")
@@ -30,11 +52,6 @@ class LambdaTest(base_test.BaseTest):
         # Verified added item
         added_item = driver.find_element_by_xpath("//span[@class='done-false']").text
         print (added_item)
-        self.assertEqual ("Yey, Let's add it to list", added_item )
-        if added_item in ":
-            return True
-        else:
-            return False
 
 if __name__ == "__main__":
     unittest.main()
