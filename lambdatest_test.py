0d1abad0fc5a1bde1248cf93436a47c65601186b
Apoorvlt <62379718+Apoorvlt@users.noreply.github.com>
2020-10-26 16:19:03 +0530

    Add files via upload

---
+++ lambdatest_test.py
@@ -0,0 +1,62 @@
+import unittest, time, re
+import base_test
+from selenium import webdriver
+ 
+from selenium.webdriver.common.keys import Keys
+ 
+class LambdaTest(base_test.BaseTest):
+ 
+    def test_unit_user_should_able_to_add_item(self):
+ 
+        # try:
+ 
+        driver = self.driver
+ 
+        # Url
+ 
+        driver.get(self.base_url)
+ 
+        # Click on check box
+ 
+        check_box_one = driver.find_element_by_name("li1")
+ 
+        check_box_one.click()
+ 
+        # Click on check box
+ 
+        check_box_two = driver.find_element_by_name("li2")
+ 
+        check_box_two.click()
+ 
+        # Enter item in textfield
+ 
+        textfield = driver.find_element_by_id("sampletodotext")
+ 
+        textfield.send_keys("Yey, Let's add it to list")
+ 
+        # Click on add button
+ 
+        add_button = driver.find_element_by_id("addbutton")
+ 
+        add_button.click()
+ 
+        # Verified added item
+ 
+        added_item = driver.find_element_by_xpath("/html/body/div/div/div/ul/li[6]/span").text
+ 
+        print (added_item)
+
+        #Assertion 
+        if "Yey, Let's add it to list" in added_item:
+            driver.execute_script("lambda-status=passed")
+
+        else:
+            driver.execute_script("lambda-status=failed")
+            
+        
+ 
+
+ 
+if __name__ == "__main__":
+ 
+    unittest.main()
